41750be2686faf9f6a47de3af3b7cff4f9c32fbd
gabrieleMerra <blumarling2@msn.com>
2018-11-14 09:51:30 +0100

    update

--- wp-content/themes/liltmilano/functions.php
+++ wp-content/themes/liltmilano/functions.php
@@ -1,14 +1,50 @@
 <?php
 
-wp_schedule_single_event( strtotime('17:25:00'), 'parteMail' );
+function get_submissions() {
+  $field_models = Ninja_Forms()->form( 2 )->get_fields();
+  $allSubs_model = Ninja_Forms()->form( 2 )->get_subs();
 
+  $fields = array();
+  $fields_label = array();
+  $submissions = array();
 
+  // loop per prendere gli id e le label dei campi
+  foreach($field_models as $key => $value) {
+    array_push($fields, $value->get_setting('key'));
+    array_push($fields_label, $value->get_setting('label'));
 
-function parteMailFunct() {
-  wp_mail( 'gabriele.merra@gmail.com', 'Automatic email', 'Automatic scheduled email from WordPress.');
+  }
+
+  // loop per tirare giu tutte le submissions
+  foreach($allSubs_model as $key => $submission_model) {
+    $obj1 = new \stdClass;
+    foreach ($fields_label as $key => $value) {
+      $obj1->$value = $submission_model->get_field_value($fields[$key]);
+    }
+    array_push($submissions, $obj1);
+  }
+
+  return $submissions;
+
 }
 
-add_action( 'parteMail', 'parteMailFunct' );
+// function schedulingSend() {
+//   $timestamp = wp_next_scheduled( 'parteMail' );
+//   if(isset($timestamp)) {
+//
+//   } else {
+//     wp_schedule_single_event( strtotime('11:32:00'), 'parteMail' );
+//   }
+// }
+//
+//
+// function parteMailFunct() {
+//   wp_mail( 'gabriele.merra@gmail.com', 'Automatic email', 'Automatic scheduled email from WordPress.');
+// }
+//
+// add_action( 'parteMail', 'parteMailFunct' );
+
+
 
 
 ?>
--- wp-content/themes/liltmilano/single.php
+++ wp-content/themes/liltmilano/single.php
@@ -12,22 +12,19 @@
 
 get_header(); ?>
 
-<div class="wrap">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+	<div>
+		<?php
+		/* Start the Loop */
+		while ( have_posts() ) : the_post();
 
 			the_content();
-			
-			endwhile; // End of the loop.
-			?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		endwhile; // End of the loop.
+		?>
+	</div>
+<?php
+
+?>
 	<?php get_sidebar(); ?>
-</div><!-- .wrap -->
 
 <?php get_footer();
